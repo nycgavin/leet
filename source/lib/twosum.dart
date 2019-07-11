98e336bcae89beaef8925432ace7bbd8180e2a13
Gavin Lee <leeg@ujafedny.org>
2019-07-11 17:44:29 -0400

    added method to solve two sum using a hash map

--- source/lib/twosum.dart
+++ source/lib/twosum.dart
@@ -1,22 +1,31 @@
-/*
-Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-*/
+import 'dart:collection';
 
+//Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 class TwoSum {
+  ///O(n * n)
   List<int> usingIteration(List<int> integers, int target) {
-    for(int i = 0; i < integers.length; i++)
-    {
-      for(int k = i + 1; k < integers.length; k++)
-      {
-        if(integers[i] + integers[k] == target)
-        {
+    for (int i = 0; i < integers.length; i++) {
+      for (int k = i + 1; k < integers.length; k++) {
+        if (integers[i] + integers[k] == target) {
           return [i, k];
         }
-
       }
     }
+  }
 
+  ///O(n)
+  List<int> usingHashTable(List<int> integers, int target) {
+    HashMap<int, int> map = HashMap<int, int>();
+
+    for (int i = 0; i < integers.length; i++) {
+      int addend = target - integers[i];
+      if (map.containsKey(addend)) {
+        return [map[addend], i];
+      }
+
+      map[integers[i]] = i;
+    }
   }
 }
--- source/test/test.dart
+++ source/test/test.dart
@@ -1,5 +1,5 @@
-import 'package:source/twosum.dart';
 import 'package:test/test.dart';
+import 'package:source/twosum.dart';
 
 //pub run test test/test.dart --pause-after-load
 
@@ -8,6 +8,7 @@ void main() {
     var array;
     setUp(() {
       array = [1, 2, 3, 5, 10];
+      
     });
 
     test('two sum problem min edge case', () {
@@ -19,6 +20,18 @@ void main() {
 
     test('two sum problem skip case', () {
       expect(TwoSum().usingIteration(array, 7), [1, 3]);
+    });
+
+    test('two sum problem min edge case solved using hashmap', (){
+      expect(TwoSum().usingHashTable(array, 3), [0,1]);
+    });
+
+    test('two sum problem max edge case solved using hashmap', (){
+      expect(TwoSum().usingHashTable(array, 15), [3,4]);
+    });
+
+    test('two sum problem skip case solved using hashmap', (){
+      expect(TwoSum().usingHashTable(array, 7), [1,3]);
     });
   });
 }
